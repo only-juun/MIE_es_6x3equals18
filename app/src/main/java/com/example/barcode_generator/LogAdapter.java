e9145d72e66581a976ac4d65bf8271b831e3ca0d
only-juun <nhj7334@icloud.com>
2021-06-12 14:18:23 +0900

    1.택배목록 화면 구현 2.택배목록 세분화 - 배송전, 배송완료 3.배송 전, 배송완료 목록 화면 구현

--- app/src/main/java/com/example/barcode_generator/LogAdapter.java
+++ app/src/main/java/com/example/barcode_generator/LogAdapter.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.jetbrains.annotations.NotNull;
 
